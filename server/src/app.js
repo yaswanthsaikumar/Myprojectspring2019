98aced2aadccbf26ac9ffc11b2f9216c49259cd0
Yaswanth <yskchowdary4@gmail.com>
2019-04-29 18:12:18 -0400

    Quit using sqlite and sequelize,switch to expresserver

--- server/src/app.js
+++ server/src/app.js
@@ -1,20 +1,19 @@
-console.log("hello")
+console.log('hello')
 
 const express = require('express')
 const bodyparser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-const {sequelize} = require('./main')
+const { sequelize } = require('./main')
 const config = require('./configurations/config.js')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyparser.json())
 app.use(cors())
-
-
-sequelize.sync({force: true})
+sequelize.sync({ force: true })
   .then(() => {
     app.listen(config.port)
-    console.log("server started on port ${config.port}")
+    // eslint-disable-next-line no-template-curly-in-string
+    console.log('server started on port ${config.port}')
   })
