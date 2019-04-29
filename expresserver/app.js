90496666c868746e73752d6633816306c0728392
Yaswanth <yskchowdary4@gmail.com>
2019-04-29 19:17:25 -0400

    changes & connection to wyvern

--- expresserver/app.js
+++ expresserver/app.js
@@ -8,9 +8,8 @@ const cors = require('cors');
 const rootRouter = require('./routes');
 
 const PORT = 3000;
-const DB_CONNECTION_STRING = 'mongodb://localhost/fitness';
-
-mongoose.connect(DB_CONNECTION_STRING);
+const DB_CONNECTION_STRING = 'mongodb://muppally1:coeweh@wyvern.cs.newpaltz.edu:27017/muppally1_db';
+mongoose.connect(DB_CONNECTION_STRING, { useNewUrlParser: true });
 const db = mongoose.connection;
 // handle mongo error
 // eslint-disable-next-line no-console
@@ -26,6 +25,7 @@ const whitelist = ['http://localhost:8080'];
 const corsOptionsDelegate = function (req, callback) {
   let corsOptions;
   if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    // eslint-disable-next-line max-len
     corsOptions = { origin: true, credentials: true }; // reflect (enable) the requested origin in the CORS response
   } else {
     corsOptions = { origin: false }; // disable CORS for this request
@@ -56,5 +56,6 @@ app.get('/', (req, res) => {
 rootRouter(app);
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`server listening on port: ${PORT}`);
 });
